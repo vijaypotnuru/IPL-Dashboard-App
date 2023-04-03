ba9eb83c26160e6c8fdeb42c940b9cfae608d01c
vijaypotnuru <vijaypotnuru123@gmail.com>
2023-04-03 15:01:16 +0000

    IPL Dashboard App

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const NotFound = () => {
+  console.log('HI')
+
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-heading">Page Not Found !!</h1>
+    </div>
+  )
+}
+
+export default NotFound
